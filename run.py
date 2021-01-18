164e2725c3e6fef823b21f8ea1c8664e3371353f
Diego Restrepo <restrepo@udea.edu.co>
2021-01-18 11:13:14 -0500

    solutions updated

--- run.py
+++ run.py
@@ -13,12 +13,13 @@ z=solutions.z
 #import warnings
 import time
 #warnings.filterwarnings("ignore")
-s=solutions.solutions(parallel=True,n_jobs=70)
+n=6
+s=solutions.solutions(parallel=True,n_jobs=5)
 st=time.time()
-#l=s(6,verbose=True,print_step=500000, nmin=-21,nmax=21,zmax=32)
-l =s(9,verbose=True,print_step=1000000,nmin=-12,nmax=12,zmax=32)
+l=s(6,verbose=True,print_step=500000, nmin=-21,nmax=21,zmax=32,max_size=100000000)
+#l =s(9,verbose=True,print_step=1000000,nmin=-12,nmax=12,zmax=32)
 print(time.time()-st)
 l=[d for d in l if d]
 df=pd.DataFrame(l)
 df['strz']=df['z'].astype(str)
-df.drop_duplicates('strz').drop('strz',axis='columns').reset_index(drop=True).to_json('s9.json')
+df.drop_duplicates('strz').drop('strz',axis='columns').reset_index(drop=True).to_json('s{}.json'.format(n))
